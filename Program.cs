2e66a32a5e0d498a941073443264871536bd0fb2
Stacy <zemcovannastasia11@gmail.com>
2022-12-15 22:48:30 +0500

    работает

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Lab
 {
-    static class Program
+    class Program
     {
-        static void Main()
+        public static void RemoveAnnagrams(List<string> array)
         {
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = i + 1; j < array.Count; j++)
+                {
+                    if (IsAnnagrams(array[i].ToCharArray(), array[j].ToCharArray()))
+                    {
+                        array[j] = " ";
+                    }
+                    
+                }
+                if (array[i] == " ")
+                {
+                    array.Remove(array[i]);
+                }
+            }
 
+        }
+
+        public static bool IsAnnagrams(char[] a, char[] b)
+        {
+            Array.Sort(a);
+            Array.Sort(b);
+            if (a.SequenceEqual(b))
+                return true;
+            else return false;
+        }
+
+        public static void Main()
+        {
+            List<string> array = new List<string> {"moon", "code", "doce", "ecod", "sun", "framer", "frmaer", "frame", "snu"};
+            
+            Console.WriteLine("Initial array:");
+            foreach (string item in array)
+                Console.Write(item + " ");
+            Console.WriteLine();
+            RemoveAnnagrams(array);
+            Console.WriteLine("Array without annagrams:");
+            foreach (string item in array)
+                Console.Write(item + " ");
         }
     }
 }
